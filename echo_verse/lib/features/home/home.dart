df770101a1d42e81759461d7ca6114bbcca74836
alaminpkk05 <mdalaminpk05@gmail.com>
2025-02-05 17:57:43 +0600

    auth done

--- echo_verse/lib/features/home/home.dart
+++ echo_verse/lib/features/home/home.dart
@@ -1,10 +1,5 @@
-import 'package:echo_verse/core/constant/const_string.dart';
-import 'package:echo_verse/core/routes/route_names.dart';
-import 'package:echo_verse/features/authentication/bloc/authentication/authentication_bloc.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -20,25 +15,19 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: Column(
-        children: [
-          Center(
-            child: TextButton(
-                onPressed: () {
-                  context.read<AuthenticationBloc>().add(SignOutEvent());
-                },
-                child: Text('Sign out')),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+           mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              
+              
+              Text('Welcome to the Home Screen',style: Theme.of(context).textTheme.titleLarge,),
+             
+            ],
           ),
-          Text(FirebaseAuth.instance.currentUser!.displayName.toString()),
-          Text(FirebaseAuth.instance.currentUser!.email.toString()),
-          Text(FirebaseAuth.instance.currentUser!.uid.toString()),
-          TextButton(
-              onPressed: () {
-                
-                context.push(RoutePath.forget,extra: accountDelete);
-              },
-              child: Text('Delete'))
-        ],
+        ),
       ),
     );
   }
--- echo_verse/lib/features/settings/screens/settings.dart
+++ echo_verse/lib/features/settings/screens/settings.dart
@@ -1,4 +1,11 @@
+import 'package:echo_verse/core/constant/const_string.dart';
+import 'package:echo_verse/core/routes/route_names.dart';
+import 'package:echo_verse/dependencies/service_locator.dart';
+import 'package:echo_verse/features/authentication/bloc/authentication/authentication_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -13,6 +20,35 @@ class _SettingsScreenState extends State<SettingsScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Settings'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            children: [
+              Text(
+                firebaseAut.currentUser!.displayName!,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+               SizedBox(
+                height: 3.h,
+              ),
+              TextButton(
+                  onPressed: () {
+                    context.read<AuthenticationBloc>().add(SignOutEvent());
+                  },
+                  child: Text('Sign out')),
+              SizedBox(
+                height: 3.h,
+              ),
+              TextButton(
+                  onPressed: () {
+                    context.push(RoutePath.forget, extra: accountDelete);
+                  },
+                  child: Text('Delete'))
+            ],
+          ),
+        ),
       ),
     );
   }
