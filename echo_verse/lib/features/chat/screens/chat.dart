a26ecd46b3d0df1d4786e6eb980e7b272065455a
Alaminpk05 <mdalaminpk05@gmail.com>
2025-02-11 01:33:13 +0600

    added receiver avatar in chat page

--- echo_verse/lib/features/chat/screens/chat.dart
+++ echo_verse/lib/features/chat/screens/chat.dart
@@ -4,6 +4,7 @@ import 'package:echo_verse/core/constant/padding_radius_size.dart';
 import 'package:echo_verse/dependencies/service_locator.dart';
 import 'package:echo_verse/features/authentication/data/model/user.dart';
 import 'package:echo_verse/features/chat/data/model/messege.dart';
+import 'package:echo_verse/features/home/screens/home.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -74,7 +75,7 @@ class _ChatScreenState extends State<ChatScreen> {
         ],
       ),
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 2.w, vertical: vertical),
+        padding: EdgeInsets.only(left:2.w,right: 2.w,bottom: vertical),
         child: Column(
           children: [
             Expanded(
@@ -92,22 +93,17 @@ class _ChatScreenState extends State<ChatScreen> {
                       return Center(child: Text("No messages yet."));
                     }
 
-                    return ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: snapshot.data!.docs.length,
-                      itemBuilder: (context, index) {
-                        var doc = snapshot.data!.docs[index];
-                        final messageId = doc.id;
+                    return Expanded(
+                      child: ListView.builder(
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index) {
+                          var doc = snapshot.data!.docs[index];
+                          final messageId = doc.id;
 
-                        ChatMessageModel data = ChatMessageModel.fromJson(
-                            doc.data() as Map<String, dynamic>);
-                        final bool isSender = data.senderId != userUid;
-                        return Align(
-                          alignment: isSender
-                              ? Alignment.centerLeft
-                              : Alignment
-                                  .centerRight, // Change to centerRight for sender
-                          child: GestureDetector(
+                          ChatMessageModel data = ChatMessageModel.fromJson(
+                              doc.data() as Map<String, dynamic>);
+                          final bool isSender = data.senderId == userUid;
+                          return GestureDetector(
                             onLongPress: () {
                               chatModalSheetWidget(
                                   message: data.content,
@@ -116,29 +112,47 @@ class _ChatScreenState extends State<ChatScreen> {
                                   senderId: userUid!,
                                   messageId: messageId);
                             },
-                            child: Container(
-                              margin: EdgeInsets.symmetric(
-                                  vertical: 5, horizontal: 10),
-                              padding: EdgeInsets.symmetric(
-                                  vertical: 10, horizontal: 14),
-                              decoration: BoxDecoration(
-                                color: Colors
-                                    .teal, // Use Colors.teal instead of teal if undefined
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              constraints: BoxConstraints(
-                                maxWidth: 70
-                                    .w, // Limits max width but allows it to grow
-                              ),
-                              child: Text(
-                                data.content,
-                                style: TextStyle(
-                                    fontSize: 16, color: Colors.white),
-                              ),
+                            child: Row(
+                              mainAxisAlignment: isSender
+                                  ? MainAxisAlignment.end
+                                  : MainAxisAlignment.start,
+                              children: [
+                                isSender
+                                    ? SizedBox.shrink()
+                                    : CircleAvatarWidget(
+                                        avatarPath: widget.user.imageUrl ??
+                                            'lib/assets/logo/robo.png',
+                                        radius: 19.sp),
+                                Container(
+                                
+                                  margin: EdgeInsets.symmetric(
+                                      vertical: 5, horizontal: 10),
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: 1.2.h, horizontal: 3.w),
+                                  decoration: BoxDecoration(
+                                    color: Colors
+                                        .teal, // Use Colors.teal instead of teal if undefined
+                                    borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(11.sp),
+                                        topRight: Radius.circular(0),
+                                        bottomLeft: Radius.circular(11.sp),
+                                        bottomRight: Radius.circular(11.sp)),
+                                  ),
+                                  constraints: BoxConstraints(
+                                      maxWidth: 50.w,
+                                      
+                                      ),
+                                  child: Text(
+                                    data.content,
+                                    style: TextStyle(
+                                        fontSize: 16, color: Colors.white),
+                                  ),
+                                ),
+                              ],
                             ),
-                          ),
-                        );
-                      },
+                          );
+                        },
+                      ),
                     );
                   },
                 ),
@@ -153,8 +167,8 @@ class _ChatScreenState extends State<ChatScreen> {
               onTapcamera: () {},
               onTapSend: () {
                 if (_messageController.text.isNotEmpty) {
-                  messageServices.sendMessage(
-                      widget.user.authId.toString(), _messageController.text);
+                  messageServices.sendMessage(widget.user.authId.toString(),
+                      _messageController.text.trim());
                 }
                 _messageController.clear();
               },
@@ -189,7 +203,7 @@ class _ChatScreenState extends State<ChatScreen> {
                   onTap: () {
                     messageServices.copyText(
                         context: context, message: message);
-                         context.pop();
+                    context.pop();
                   },
                 ),
                 ChatBottomSheetButtonWidget(
